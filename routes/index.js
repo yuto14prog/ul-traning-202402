858a7056df357cac8ab7775f13c39d2a96bf6562
yuto14prog <yuto14prog@gmail.com>
2024-03-01 14:57:56 +0900

    04.WebとDBの連携

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   const now = new Date();
-  res.render('index', { title: 'Hello World', now: now });
+  const contacts = await models.Contact.findAll();
+  res.render('index', { title: '連絡帳', now: now, contacts: contacts });
 });
 
 router.get('/about', function (req, res, next) {
@@ -15,8 +17,11 @@ router.get('/contact_form', function (req, res, next) {
   res.render('contact_form', { title: '連絡先フォーム' });
 });
 
-router.post('/contacts', function (req, res, next) {
+router.post('/contacts', async function (req, res, next) {
   console.log('posted', req.body);
+  const num = (new Date()).getTime();
+  const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
+  await contact.save();
   res.redirect('/');
 });
 
