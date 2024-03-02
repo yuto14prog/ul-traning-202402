4fa72953ee1c3f9d15e950f194f017c37abf5c4d
yuto14prog <yuto14prog@gmail.com>
2024-03-02 11:14:36 +0900

    06.セッションとCookie

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ const { ValidationError } = require('sequelize');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  req.session.view_counter = (req.session.view_counter || 0) + 1;
+  const flashMessage = req.session.flashMessage;
+  delete req.session.flashMessage;
+
   const now = new Date();
   const contacts = await models.Contact.findAll();
-  res.render('index', { title: '連絡帳', now: now, contacts: contacts });
+  res.render('index', { title: '連絡帳', now, contacts, view_counter: req.session.view_counter, flashMessage });
 });
 
 router.get('/about', function (req, res, next) {
@@ -23,6 +27,7 @@ router.post('/contacts', async function (req, res, next) {
     console.log('posted', req.body);
     const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
     await contact.save();
+    req.session.flashMessage = `新しい連絡先として「${contact.name}」さんを保存しました`;
     res.redirect('/');
   } catch (err) {
     if (err instanceof ValidationError) {
