928a2912e8431a83e4e5d25fdf06ce4e1a2c2b5c
yuto14prog <yuto14prog@gmail.com>
2024-03-04 12:55:20 +0900

    10.アソシエーションを利用したUI

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,18 @@ router.get('/', async function (req, res, next) {
   delete req.session.flashMessage;
 
   const now = new Date();
-  const contacts = await models.Contact.findAll();
-  res.render('index', { title: '連絡帳', now, contacts, view_counter: req.session.view_counter, flashMessage });
+  const contacts = await models.Contact.findAll({ include: 'category' });
+  const categories = await models.Category.findAll();
+  res.render('index', { title: '連絡帳', now, contacts, categories, view_counter: req.session.view_counter, flashMessage });
 });
 
 router.get('/about', function (req, res, next) {
   res.render('about', { title: 'About' });
 });
 
-router.get('/contact_form', function (req, res, next) {
-  res.render('contact_form', { title: '連絡先の作成', contact: {} });
+router.get('/contact_form', async function (req, res, next) {
+  const categories = await models.Category.findAll();
+  res.render('contact_form', { title: '連絡先の作成', contact: {}, categories });
 });
 
 router.get('/contacts/:id/edit', async function (req, res, next) {
@@ -28,7 +30,7 @@ router.get('/contacts/:id/edit', async function (req, res, next) {
 });
 
 router.post('/contacts', async function (req, res, next) {
-  const fields = ['name', 'email'];
+  const fields = ['name', 'email', 'categoryId'];
   try {
     console.log('posted', req.body);
     if (req.body.id) {
@@ -37,12 +39,18 @@ router.post('/contacts', async function (req, res, next) {
       // contact.name = req.body.name;
       // contact.email = req.body.email;
       contact.set(req.body);
+      if (contact.categoryId == '') {
+        contact.categoryId = null;
+      }
       await contact.save({ fields });
       req.session.flashMessage = `「${contact.name}」さんを更新しました`;
     } else {
       // insert
       // const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
       const contact = models.Contact.build(req.body);
+      if (contact.categoryId == '') {
+        contact.categoryId = null;
+      }
       await contact.save({ fields });
       req.session.flashMessage = `新しい連絡先として「${contact.name}」さんを保存しました`;
     }
@@ -50,7 +58,8 @@ router.post('/contacts', async function (req, res, next) {
   } catch (err) {
     if (err instanceof ValidationError) {
       const title = (req.body.id) ? '連絡先の更新' : '連絡先の作成';
-      res.render(`contact_form`, { title, contact: req.body, err: err });
+      const categories = await models.Category.findAll();
+      res.render(`contact_form`, { title, contact: req.body, err: err, categories });
     } else {
       throw err;
     }
@@ -63,6 +72,12 @@ router.post('/contacts/:id/delete', async function (req, res, next) {
   await contact.destroy();
   req.session.flashMessage = `「${contact.name}」さんを削除しました`;
   res.redirect('/');
+});
+
+router.get('/categories/:id', async function (req, res, next) {
+  const category = await models.Category.findByPk(req.params.id);
+  const contacts = await category.getContacts({ include: 'category' });
+  res.render('category', { title: `カテゴリ ${category.name}`, category, contacts });
 });
 
 module.exports = router;
